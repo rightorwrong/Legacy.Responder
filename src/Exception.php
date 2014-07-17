3f03ce04a2683b24531f49086246c08fbde8f361
jacobsantos <jacobsantos@jacobsantos.com>
2014-07-16 21:29:18 -0500

    Add project level exception class.

---
+++ src/Exception.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jacob Santos
+ * Date: 7/16/14
+ * Time: 9:03 PM
+ */
+
+namespace EvolveCMS\Responder\WP;
+
+use Exception as BaseException;
+
+/**
+ * Class Exception
+ *
+ * Project level exception. Every exception extends this exception class.
+ *
+ * @package EvolveCMS\Responder\WP
+ */
+class Exception extends BaseException { }
